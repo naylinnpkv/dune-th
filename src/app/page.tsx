14cdc450e548d5fbc9a210dc11ee8b7f2de3b751
naylinn-zealthy <naylinn.pkv@gmail.com>
2025-05-17 13:07:56 -0400

    test direct fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import Assets from "@/components/assets/Assets";
 
 export default async function Home() {
   console.log(process.env.NEXT_PUBLIC_BASE_URL);
-  const res = await fetch(
-    `https://rest.coincap.io/v3/assets?limit=20&offset=0&apiKey=${process.env.COINCAP_API_KEY}`
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/assets`);
   const data = await res.json();
   const assets = data.data;
   console.log(assets);
